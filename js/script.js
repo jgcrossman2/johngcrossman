ce36dfb42fe377ee9c76e3d52c80f896e1abe31c
John Crossman <jgcrossman2@gmail.com>
2020-04-22 21:53:14 -0700

    add some javascript

---
+++ js/script.js
@@ -0,0 +1,77 @@
+var sectionOne = document.getElementById('fluxfinancial');
+var sectionTwo = document.getElementById('fintechblockchain');
+var sectionThree = document.getElementById('asianinsto');
+var sectionFour = document.getElementById('USretail');
+var sectionFive = document.getElementById('education');
+var sectionSix = document.getElementById('licenses');
+var sectionSeven = document.getElementById('skills');
+var button1 = document.getElementById('b1');
+var button2 = document.getElementById('b2');
+var button3 = document.getElementById('b3');
+var button4 = document.getElementById('b4');
+var button5 = document.getElementById('b5');
+var button6 = document.getElementById('b6');
+var button7 = document.getElementById('b7');
+button1.addEventListener('click', function() {
+    sectionOne.style.display = 'block';
+    sectionTwo.style.display = 'none';
+    sectionThree.style.display = 'none';
+    sectionFour.style.display = 'none';
+    sectionFive.style.display = 'none';
+    sectionSix.style.display = 'none';
+    sectionSeven.style.display = 'none';
+});
+button2.addEventListener('click', function() {
+    sectionOne.style.display = 'none';
+    sectionTwo.style.display = 'block';
+    sectionThree.style.display = 'none';
+    sectionFour.style.display = 'none';
+    sectionFive.style.display = 'none';
+    sectionSix.style.display = 'none';
+    sectionSeven.style.display = 'none';
+});
+button3.addEventListener('click', function() {
+    sectionOne.style.display = 'none';
+    sectionTwo.style.display = 'none';
+    sectionThree.style.display = 'block';
+    sectionFour.style.display = 'none';
+    sectionFive.style.display = 'none';
+    sectionSix.style.display = 'none';
+    sectionSeven.style.display = 'none';
+});
+button4.addEventListener('click', function() {
+    sectionOne.style.display = 'none';
+    sectionTwo.style.display = 'none';
+    sectionThree.style.display = 'none';
+    sectionFour.style.display = 'block';
+    sectionFive.style.display = 'none';
+    sectionSix.style.display = 'none';
+    sectionSeven.style.display = 'none';
+});
+button5.addEventListener('click', function() {
+    sectionOne.style.display = 'none';
+    sectionTwo.style.display = 'none';
+    sectionThree.style.display = 'none';
+    sectionFour.style.display = 'none';
+    sectionFive.style.display = 'block';
+    sectionSix.style.display = 'none';
+    sectionSeven.style.display = 'none';
+});
+button6.addEventListener('click', function() {
+    sectionOne.style.display = 'none';
+    sectionTwo.style.display = 'none';
+    sectionThree.style.display = 'none';
+    sectionFour.style.display = 'none';
+    sectionFive.style.display = 'none';
+    sectionSix.style.display = 'block';
+    sectionSeven.style.display = 'none';
+});
+button7.addEventListener('click', function() {
+    sectionOne.style.display = 'none';
+    sectionTwo.style.display = 'none';
+    sectionThree.style.display = 'none';
+    sectionFour.style.display = 'none';
+    sectionFive.style.display = 'none';
+    sectionSix.style.display = 'none';
+    sectionSeven.style.display = 'block';
+});
